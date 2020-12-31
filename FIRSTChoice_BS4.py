5b987cfc6c2c93c3daceb803b7e64e0f1ea37518
FRC5414 <pearlandFRC@gmail.com>
2020-12-31 13:32:15 -0600

    Update FIRSTChoice_BS4.py
    Lazy update - split out First choice Part ID into it's own column and removed some extraneous comments.

--- FIRSTChoice_BS4.py
+++ FIRSTChoice_BS4.py
@@ -3,29 +3,20 @@ from urllib.request import urlopen
 import csv
 
 # specify the url
-quote_page = 'https://firstchoicebyandymark.com/everything'
+Everythingpage = 'https://firstchoicebyandymark.com/everything'
 
 # query the website and return the html to the variable ‘page’
-page = urlopen(quote_page)
+page = urlopen(Everythingpage)
 
 soup = BeautifulSoup(page, 'html.parser')
 productids = []
-# mydivs = soup.findAll("div", {"class":'product-item'})
 mydivs = soup.select('div.product-item')
+
 for link in mydivs:
     productid = link.find('a').attrs['href']
     productids.append(productid)
-    title = link.find('a').attrs['title']
-    # print(href.attrs['href'])
-    # print(productid)
-    # print(title)
-    priceinfo = link.select('div.prices')
-    # price = priceinfo.find('price actual-price')
-    # print(priceinfo)
-    # price = link.find('price')
-    # print(price)
 
-headers = ["Webpage", "Item", "Description", "Source", "Source Part Number", "onhand", "onhold", "available", "Credits Price", "Max Qty"]
+headers = ["Webpage", "FC Part ID", "Item", "Description", "Source", "Source Part Number", "onhand", "onhold", "available", "Credits Price", "Max Qty"]
 rows = [headers]
 for id in productids:
     print(id)
@@ -69,40 +60,9 @@ for id in productids:
     except:
         maxqty = "NA"
 
-    # if (id == "/fc-fan12"):
-    #     print("Name: " + name)
-    #     print("Description: " + description)
-    #     print("Source: " + source)
-    #     print("Source Part Number: " + sourcePN.replace("#", ''))
-    #     print("Onhand: " + onhand)
-    #     print("Onhold: " + onhold)
-    #     print("Available: " + available)
-    #     print("Price: " + price)
-    #     print(maxqty)
-        # print(idsoup.p.string)
-
-        # print(idsoup)
-
-
-    row = [idurl, name, description, source, sourcePN, onhand, onhold, available, price, maxqty]
+    row = [idurl, id[1:], name, description, source, sourcePN, onhand, onhold, available, price, maxqty]
     rows.append(row)
-    # overview = idsoup.select('div.overview')
-    # name = idsoup.find('h1')
-    # print(name.text.strip())
-    # # for h in idsoup.find_all('h1'):
-    # #     print(h.text.strip())
-    # # print(source)
-    # for item in overview:
-    #     print("Item: \r\n~~~~~~~~~~\r\n")
-    #     print(item)
-    #     source = item.find('a').attrs['title']
-    #     print("Source: "+source)
-    #     input()
-    # print(overview)
-    # # name = overview.find('h1')
 
 with open("FirstChoice.csv", mode='w', newline='') as file:
     file_writer = csv.writer(file)
     file_writer.writerows(rows)
-# name = name_box.text.strip()
-# print(name)
